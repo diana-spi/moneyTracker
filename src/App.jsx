7bc07e893dc99455dfbb0780a30f318e489c8d0e
Diana Spivak <d0990683511@gmail.com>
2022-07-12 15:32:44 +0300

    Created project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import MainPage from "./features/MainPage/MainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import "./MainPage.scss";
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <Header />
+    </div>
+  );
+}
+
+export default MainPage;
