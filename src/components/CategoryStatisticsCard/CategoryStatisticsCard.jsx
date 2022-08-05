30d5af5b00a1d545449d10d2e4f499f63ff7c97b
Diana Spivak <d0990683511@gmail.com>
2022-08-05 10:44:13 +0300

    Added animation

--- src/components/CategoryStatisticsCard/CategoryStatisticsCard.jsx
+++ src/components/CategoryStatisticsCard/CategoryStatisticsCard.jsx
@@ -1,12 +1,27 @@
 import "./CategoryStatisticsCard.scss";
 import { round } from "lodash";
+import { useEffect, useState } from "react";
 
 function CategoryStatisticsCard({ category, sum, fillPercantage, transactionColor }) {
+  const [animatedFill, setAnimatedFill] = useState(0);
+
+  useEffect(() => {
+    setAnimatedFill(0);
+    let currentValue = 0;
+    const fillInterval = setInterval(() => {
+      currentValue += 4;
+      setAnimatedFill(currentValue);
+      if (currentValue >= fillPercantage) {
+        clearInterval(fillInterval);
+      }
+    }, 1);
+  }, [category]);
+
   return (
     <div className="category-statistics-card">
       <div
         className="category-statistics-card__fill"
-        style={{ width: `calc(${fillPercantage}% - 20px)`, backgroundColor: transactionColor }}
+        style={{ width: `calc(${animatedFill}% - 20px)`, backgroundColor: transactionColor }}
       ></div>
       <div className="category-statistics-card__content">
         <div className="category-statistics-card__category">{category}</div>
--- src/components/TotalBlock/TotalBlock.jsx
+++ src/components/TotalBlock/TotalBlock.jsx
@@ -1,11 +1,15 @@
 import "./TotalBlock.scss";
 import transactionsData, { transactionTypes } from "../../data/transactions";
 import { Typography } from "@mui/material";
-import { round } from "lodash";
+import { round, sum } from "lodash";
 import moment from "moment";
 import intervalVariants from "../../constans/filterValues";
+import { useState, useEffect } from "react";
 
 function TotalBlock({ selectedFilter, selectedBankAcc }) {
+  const [animatedIncomeSum, setAnimatedIncomeSum] = useState(0);
+  const [animatedOutcomeSum, setAnimatedOutcomeSum] = useState(0);
+
   const getStartedDate = () => {
     let firstDate = moment();
 
@@ -28,6 +32,51 @@ function TotalBlock({ selectedFilter, selectedBankAcc }) {
     return firstDate;
   };
 
+  const calcSum = (trType) => {
+    return round(
+      transactionsData
+        .filter(
+          (transaction) =>
+            selectedBankAcc.length === 0 ||
+            selectedBankAcc.map((account) => account.toLowerCase()).includes(transaction.account)
+        )
+        .filter((transaction) => transaction.date >= getStartedDate().toDate() && transaction.date <= moment().toDate())
+        .filter((trans) => trans.type === trType)
+        .reduce((acc, transaction) => acc + transaction.sum, 0),
+      2
+    );
+  };
+  const totalIncome = calcSum(transactionTypes.INCOME);
+  const totalOutcome = calcSum(transactionTypes.OUTCOME);
+
+  useEffect(() => {
+    setAnimatedIncomeSum(0);
+    let currentValue = 0;
+    const step = totalIncome / 50;
+    const sumInterval = setInterval(() => {
+      if (currentValue + step >= totalIncome) {
+        setAnimatedIncomeSum(totalIncome);
+        return clearInterval(sumInterval);
+      }
+      currentValue += step;
+      setAnimatedIncomeSum(currentValue);
+    }, 2);
+  }, [totalIncome]);
+
+  useEffect(() => {
+    setAnimatedOutcomeSum(0);
+    let currentValue = 0;
+    const step = totalOutcome / 50;
+    const sumInterval = setInterval(() => {
+      if (currentValue + step >= totalOutcome) {
+        setAnimatedOutcomeSum(totalOutcome);
+        return clearInterval(sumInterval);
+      }
+      currentValue += step;
+      setAnimatedOutcomeSum(currentValue);
+    }, 2);
+  }, [totalOutcome]);
+
   return (
     <div className="total-block">
       <div className="total-block__column">
@@ -36,46 +85,11 @@ function TotalBlock({ selectedFilter, selectedBankAcc }) {
         </Typography>
         <div className="total-block__row">
           <div className="total-block__income">
-            <div className="total-block__income-sum">
-              {round(
-                transactionsData
-                  .filter(
-                    (transaction) =>
-                      selectedBankAcc.length === 0 ||
-                      selectedBankAcc.map((account) => account.toLowerCase()).includes(transaction.account)
-                  )
-                  .filter(
-                    (transaction) =>
-                      transaction.date >= getStartedDate().toDate() && transaction.date <= moment().toDate()
-                  )
-                  .filter((trans) => trans.type === transactionTypes.INCOME)
-                  .reduce((acc, transaction) => acc + transaction.sum, 0),
-                2
-              ).toFixed(2)}
-              $
-            </div>
+            <div className="total-block__income-sum">{animatedIncomeSum.toFixed(2)}$</div>
             <div className="total-block__income-title">Income</div>
           </div>
           <div className="total-block__outcome">
-            <div className="total-block__outcome-sum">
-              -
-              {round(
-                transactionsData
-                  .filter(
-                    (transaction) =>
-                      selectedBankAcc.length === 0 ||
-                      selectedBankAcc.map((account) => account.toLowerCase()).includes(transaction.account)
-                  )
-                  .filter(
-                    (transaction) =>
-                      transaction.date >= getStartedDate().toDate() && transaction.date <= moment().toDate()
-                  )
-                  .filter((trans) => trans.type === transactionTypes.OUTCOME)
-                  .reduce((acc, transaction) => acc + transaction.sum, 0),
-                2
-              ).toFixed(2)}
-              $
-            </div>
+            <div className="total-block__outcome-sum">-{animatedOutcomeSum.toFixed(2)}$</div>
             <div className="total-block__outcome-title">Outcome</div>
           </div>
         </div>
